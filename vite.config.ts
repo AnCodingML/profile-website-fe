091b679cdcdaeae701e4fc9065093e27259b0ae9
Andi <{an.ekanugraha@gmail.com}>
2024-07-11 11:58:05 +0700

    rev vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Ganti dengan port yang kamu inginkan
+    host: '0.0.0.0', // Mengikat pada semua antarmuka jaringan
+    watch: {
+      usePolling: false, // Disable polling to avoid unnecessary file watching
+    },
+  },
 })
