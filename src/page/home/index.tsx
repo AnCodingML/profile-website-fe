29f1c91d88a10eec5d4a99074cee9a2a62e93afa
Andi <{an.ekanugraha@gmail.com}>
2024-07-13 12:53:23 +0700

    add skill set

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -7,7 +7,7 @@ import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import { useSpring, useTrail, animated, easings } from '@react-spring/web';
 import { Container, Image } from "react-bootstrap";
 import Satelit from '../../assets/icon/satelit.svg';
-import Profile from '../../assets/img/photo.jpg';
+import Backend from '../../assets/icon/backend.svg';
 import Frontend from '../../assets/icon/frontend.svg';
 import Javascript from '../../assets/icon/javascript.svg';
 import Typescript from '../../assets/icon/Typescript.svg';
@@ -16,6 +16,13 @@ import Html5 from '../../assets/icon/HTML5.svg';
 import Css from '../../assets/icon/CSS.svg'
 import Bootstrap from '../../assets/icon/bootstrap.svg';
 import Tailwind from '../../assets/icon/tailwind.svg';
+import Nodejs from '../../assets/icon/node-js.svg'
+import Express from '../../assets/icon/express-js.svg'
+import Postgres from '../../assets/icon/postgres.svg'
+import Mysql from '../../assets/icon/mysql.svg'
+import Git from '../../assets/icon/git.svg'
+import Alibaba from '../../assets/icon/alibaba-cloud.svg'
+import Devops from '../../assets/icon/devops.svg'
 
 import {
   Box,
@@ -74,10 +81,6 @@ function AboutUs() {
     loop: true,
   });
 
-  const handleDialogChange = () => {
-    // window.open(`localhost:4000${Profile}`, '_blank');
-  }
-
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadFull(engine);
@@ -88,8 +91,8 @@ function AboutUs() {
 
   return (
     <>  
-      <Parallax ref={parallax} pages={3} style={{ background: '#0E0E52', fontFamily:'Orbitron' }}>
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+      <Parallax ref={parallax} pages={4} style={{ background: '#0E0E52', fontFamily:'Orbitron' }}>
+        {/* <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} /> */}
         <ParallaxLayer offset={0} speed={0} factor={3} style={{ position: 'relative' }}>
           {init && (
             <Particles className="position-absolute" options={particlesOptions as unknown as ISourceOptions} />
@@ -144,11 +147,11 @@ function AboutUs() {
           </Container>
         </ParallaxLayer>
         <ParallaxLayer offset={1}>
-          <Container className="d-lg-flex d-grid gap-4">
-            <div className="skill-set p-sm-5 p-4 rounded-4">
+          <Container className="d-lg-flex flex-wrap d-grid gap-4 justify-content-center">
+            <div className="skill-set p-sm-5 p-4 rounded-4" style={{maxWidth:'416px'}}>
               <div className="fs-1 text-white fw-bold rounded-4 text-center">Frontend</div>
               <div className="d-flex justify-content-center  mt-4">
-                <Image src={Frontend} alt="front-end"/>
+                <Image src={Frontend} alt="front-end" height={100}/>
               </div>
               <div className="text-white lh-lg text-justify mt-5">
                 As a front-end developer, I am skilled at building responsive and interactive user interfaces using React and TypeScript, and have a deep understanding of modern web design principles.
@@ -160,14 +163,38 @@ function AboutUs() {
                 <Image src={Typescript} height={50}/>
                 <Image src={React} height={50}/>
                 <Image src={Bootstrap} height={50}/>
-                <Image src={Tailwind} width={50}/>
+                <Image className="align-self-center" src={Tailwind} width={50}/>
               </div>
             </div>
-            <div className="skill-set p-4 text-center rounded-4">
-              <div className="fs-3 text-white fw-bold">Backend</div>
+            <div className="skill-set p-sm-5 p-4 rounded-4" style={{maxWidth:'416px'}}>
+              <div className="fs-1 text-white fw-bold rounded-4 text-center">Backend</div>
+              <div className="d-flex justify-content-center  mt-4">
+                <Image src={Backend} alt="back-end" height={100}/>
+              </div>
+              <div className="text-white lh-lg text-justify mt-5">
+                Sebagai developer back-end, saya ahli dalam pembuatan API RESTful, manajemen database (SQL dan NoSQL), ORM, autentikasi dan otorisasi (JWT, session). Dokumentasi API dan Unit Testing.
+              </div>
+              <div className="d-flex justify-content-center gap-4 flex-wrap mt-5 align-items-end">
+                <Image src={Javascript} height={50}/>
+                <Image src={Typescript} height={50}/>
+                <Image className="bg-white rounded" src={Nodejs} height={50}/>
+                <Image className="bg-white p-1 rounded" src={Express} height={50}/>
+                <Image src={Postgres} height={50}/>
+                <Image className="bg-white p-1 rounded" src={Mysql} height={50}/>
+                <Image src={Git} height={50}/>
+              </div>
             </div>
-            <div className="skill-set p-4 text-center rounded-4">
-              <div className="fs-3 text-white fw-bold">DevOps</div>
+            <div className="skill-set p-sm-5 p-4 rounded-4" style={{maxWidth:'416px'}}>
+              <div className="fs-1 text-white fw-bold rounded-4 text-center">DevOps</div>
+              <div className="d-flex justify-content-center  mt-4">
+                <Image src={Devops} alt="back-end" height={100}/>
+              </div>
+              <div className="text-white lh-lg text-justify mt-5">
+                Mengelola server Linux pada Alibaba Cloud dan CI/CD dengan GitHub Actions, serta sedang mempelajari manajemen konfigurasi, IaC, kontainerisasi dan orkestrasi, serta monitoring dan logging.
+              </div>
+              <div className="d-flex justify-content-center gap-4 flex-wrap mt-5 align-items-end">
+                <Image src={Alibaba} height={50}/>
+              </div>
             </div>
           </Container>
         </ParallaxLayer>
